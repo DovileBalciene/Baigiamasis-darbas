a70676926db29584b4c4281c04cdb85e4652d06e
dovil <doviliukas785@gmail.com>
2025-05-10 21:27:53 +0300

    pandas analize, naujas kriterijus:auksta/zema

---
+++ Duomenu_apdorojimas.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+from io import BytesIO
+file_path = 'C:/Users/dovil/PycharmProjects/Baigiamasis_darbas/Oil (WTI)_10_26_20-03_07_22.csv'
+df = pd.read_csv(file_path)
+# print(df)
+
+""" Sutvarkome datos formata"""
+df['Date'] = pd.to_datetime(df['Date'], format='mixed')
+
+
+vidutine_kaina_nuo_pradines_reiksmes = df["Open"].mean()
+print(vidutine_kaina_nuo_pradines_reiksmes.round(2))
+
+auksciausia_kaina_pagal_data = df.groupby(['Date'])['High'].max(5)
+print(auksciausia_kaina_pagal_data)
+
+
+maziausia_kaina = df.groupby('Date')['Low'].min().head(10)
+print(maziausia_kaina)
+
+df['kainu_skirtumas_nuo_pradines'] = df['Close'] - df['Open']
+print(df['kainu_skirtumas_nuo_pradines'])
+
+procentinis_pasiskirstymas = df.groupby('Open')['kainu_skirtumas_nuo_pradines'].apply(lambda x: x / 100).head(5)
+print(procentinis_pasiskirstymas)
+
+def karegorija(kaina):
+    if kaina > 100:
+        return 'Per didele'
+    elif kaina < 70:
+        return 'Zema kaina'
+    else:
+        return 'Vidutine'
+
+df['kategorija'] = df['Close'].apply(karegorija)
+print(df['kategorija'].value_counts())
+print(df[['Close', 'Date', 'kategorija']])
