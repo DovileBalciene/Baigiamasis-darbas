b3bdb8a9c0b6e4e4456b6e91b0d07e26aee9d55d
dovil <doviliukas785@gmail.com>
2025-05-12 11:50:19 +0300

    paliktas vienas grafikas

--- Naftos_kainu_vizualizacija.py
+++ Naftos_kainu_vizualizacija.py
@@ -7,20 +7,20 @@ from sklearn.preprocessing import LabelEncoder
 
 file_path = 'C:/Users/dovil/PycharmProjects/Baigiamasis_darbas/Oil (WTI)_10_26_20-03_07_22.csv'
 df = pd.read_csv(file_path)
-# print(df)
 
 
-# df['Year'] = pd.to_datetime(df['Date']).dt.year
-# print(df['Year'])
+
+df['Year'] = pd.to_datetime(df['Date']).dt.year
+print(df['Year'])
 
 
-# plt.figure(figsize=(12, 6))
-# plt.plot(df["Year"], df["High"], marker='o', linestyle='-')
-# plt.title(" Naftos kainu augimas pagal metu")
-# plt.xlabel("Metai")
-# plt.ylabel("Kainu augimas")
-# plt.grid(True)
-# plt.show()
+plt.figure(figsize=(12, 6))
+plt.plot(df["Year"], df["High"], marker='o', linestyle='-')
+plt.title(" Naftos kainu augimas pagal metu")
+plt.xlabel("Metai")
+plt.ylabel("Kainu augimas")
+plt.grid(True)
+plt.show()
 
 
 
