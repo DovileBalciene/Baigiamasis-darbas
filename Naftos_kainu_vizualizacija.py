ab2ebd2ef9b5ec7b868375cfab2ae417ae0857a5
dovil <doviliukas785@gmail.com>
2025-05-16 11:08:29 +0300

    Įrašytas komentaras

--- Naftos_kainu_vizualizacija.py
+++ Naftos_kainu_vizualizacija.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder
 
-file_path = 'C:/Users/dovil/PycharmProjects/Baigiamasis_darbas/Oil (WTI)_10_26_20-03_07_22.csv'
+file_path = 'C:/Users/dovil/PycharmProjects/Baigiamasis_darbas/Nafta_nuo_2015_01_01_iki_2025_04_30.csv'
 df = pd.read_csv(file_path)
 
 
@@ -13,7 +13,7 @@ df = pd.read_csv(file_path)
 df['Year'] = pd.to_datetime(df['Date']).dt.year
 print(df['Year'])
 
-
+# Vizualizacija
 plt.figure(figsize=(12, 6))
 plt.plot(df["Year"], df["High"], marker='o', linestyle='-')
 plt.title(" Naftos kainu augimas pagal metu")
