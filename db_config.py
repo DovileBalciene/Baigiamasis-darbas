a7fa276e3e3eb457c1b9d2e6443a9224b534616f
dovil <doviliukas785@gmail.com>
2025-05-18 22:17:35 +0300

    Failo tvarkymas

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
 DB_HOST = "localhost"  # Change if using an external database server
 DB_USER = "postgres"  # Your PostgreSQL username
-DB_PASSWORD = "123456A"  # Your PostgreSQL password
+DB_PASSWORD = "369963369"  # Your PostgreSQL password
 DB_NAME = "Naftos_kaina"  # The name of your database
 DB_PORT = 5432  # Def
